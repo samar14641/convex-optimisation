7944ea922b8d55a8986028ef8af8b43fdbb835df
Samar Dikshit <samard05@gmail.com>
2020-07-09 23:08:16 -0400

    interval bisection done

--- optimisation1D.py
+++ optimisation1D.py
@@ -1,15 +1,36 @@
 import numpy as np
 import pandas as pd
+from pprint import pprint
 
 
-def intervalBisection(tol, low, high):
-    continue
+def derivative(x):  # f(x) = x^2, hence f'(x) = 2x
+    return 2 * x
+
+def intervalBisection(tol, low, high):  # based on the interval bisection algorithm in the notes
+    i = 0  # iterations
+    a, b = low, high 
+    deri = derivative(high)  # deri = f'(high)
+
+    while (b - a) * deri > tol:
+        if derivative((a + b) / 2) > 0:
+            b = (a + b) / 2
+        else:
+            a = (a + b) / 2
+        
+        i += 1
+
+        if i == 1000:  # limit to 1000 iterations
+            break
+
+    return i
 
 def goldenSectionSearch(tol, low, high):
-    continue
+    pass
 
 def main():
     tolerance = np.linspace(1e-08, 1, num = 50, endpoint = True)  # get tolerance values for each run
+
+    results = []
 
     for tol in tolerance:  # this loop runs 50 times i.e. len of tolerance
         # get search range from a uniform distro
@@ -17,6 +38,13 @@ def main():
         high = np.random.uniform(0, 10)  # high ~ U[0, 10]
         
         ibIter = intervalBisection(tol, low, high)
-        gssIter = goldenSectionSearch(tol, low, high)
+        # print(ibIter)
+        # gssIter = goldenSectionSearch(tol, low, high)
 
-        
+        # results.append((tol, ibIter, gssIter))
+
+    # df = pd.DataFrame(results, columns = ['tol', 'ibIter', 'gssIter'])
+
+    # pprint(df)
+
+main()
